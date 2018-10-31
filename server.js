638b996f08eae5bc71fd31aa631994db4ad22e0a
Khizer Younas <m.khizeryounas@gmail.com>
2018-10-31 19:43:41 +0500

    edited

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const path = require("path");
 const app = express();
 let port = 5000;
 
-app.use("/app", express.static(path.join(__dirname, "app")));
+app.use(
+  "/app",
+  express.static(path.join(__dirname, "app"), {
+    etag: false
+  })
+);
 app.use("/assets", express.static(path.join(__dirname, "assets")));
 app.use(
   "/bower_components",
